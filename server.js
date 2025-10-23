369171285303f567c8ac155f5ab60eccbc420db7
Pratyush <pratyushgupta500@gmail.com>
2025-10-24 01:48:23 +0530

    main | adding middleware and html files

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
+//Get current path
+const __filename = url.fileURLToPath(import.meta.url); //in CommonJS module we get these 2 var bydefault
+const __dirname = path.dirname(__filename);
 
+const server = http.createServer(async (req, res) => {
 
     console.log(req.url);
     console.log(req.method);
-
     // res.setHeader('content-type','text/html');     //text/plain
     // res.statusCode = 404;
     // res.writeHead(200, {'content-type':'application/json'})
@@ -18,18 +23,27 @@ const server = http.createServer((req, res) => {
 
     try {
         if (req.method === 'GET') {
+            let filePath;
             if (req.url === '/') {
-                res.writeHead(200, { 'content-type': 'text/html' })
-                res.end(`<h1>Home</h1>`)
+                // res.writeHead(200, { 'content-type': 'text/html' })
+                // res.end(`<h1>Home</h1>`)  instead this send html doc
+                filePath = path.join(__dirname, 'public', 'index.html');
             }
             else if (req.url === '/about') {
-                res.writeHead(200, { 'content-type': 'type/html' })
-                res.end(`<h1>About</h1>`)
+                // res.writeHead(200, { 'content-type': 'type/html' })
+                // res.end(`<h1>About</h1>`)
+                filePath = path.join(__dirname, 'public', 'about.html');
             }
             else {
-                res.writeHead(404, { 'content-type': 'type/html' })
-                res.end(`Not found`)
+                // res.writeHead(404, { 'content-type': 'type/html' })
+                // res.end(`Not found`)
+                throw new Error("Not found")
             }
+
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
         }
         else {
             console.log(123)
